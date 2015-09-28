3974fb824bcf4beac674b5e1b8ecb6354458cfe0
Alex <whitedawn1@hotmail.com>
2015-09-28 11:02:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/angel/criminalintent/CrimeActivity.java
@@ -0,0 +1,30 @@
+package com.example.angel.criminalintent;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.Fragment;
+
+public class CrimeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+
+
+}
